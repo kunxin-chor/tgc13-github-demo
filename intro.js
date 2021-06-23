e92618960917d7ad72c6682ed69a07c85498e447
kunxin-chor <chorkunxin@yahoo.com>
2021-06-23 13:08:36 +0000

    Addd JS example

---
+++ intro.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+// values
+
+// integer
+42;
+
+// float
+3.14;
+
+// strings
+"the quick brown fox jumps over the lazy dog";
+
+// boolean
+true;
+
+console.log("Hello World");
+
+// use let to define a NEW variable
+let firstName  = "Paul";
+console.log(firstName);
+
+// reassign to an existing variable
+firstName = "Chandy";
+
+let age = prompt("Please enter your age");
+console.log("You are", age, "years old");
